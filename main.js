76251b70e0fb8b8fefca2864de484c9c498cab58
Obinna Ezeka <obinnae815@gmail.com>
2026-02-07 13:54:22 -0800

    fixed the reload fetch feature and & styling

--- main.js
+++ main.js
@@ -1,25 +1,9 @@
-// document.querySelector('#btn').addEventListener('click', getFetch)
-
-// function getFetch(){
-//   const choice = document.querySelector('input').value
-//   // console.log(choice)
-//   const url = `https://api.themoviedb.org/3/discover/movie?api_key=3c111c26b6ae2ffe0c8748c0b03b23a2&results=${choice}`
-
-//   fetch(url)
-//       .then(res => res.json()) // parse response as JSON
-//       .then(data => {
-//         console.log(data)
-//       })
-//       .catch(err => {
-//           console.log(`error ${err}`)
-//       });
-// }
-
+// Created event listener for search button to search when clicked
 document.querySelector('#btn').addEventListener('click', getFetch);
 
 function getFetch() {
+  // calls TMDB API to find requested movie
   const choice = document.querySelector('input').value.trim();
-
   const url = `https://api.themoviedb.org/3/search/movie?api_key=3c111c26b6ae2ffe0c8748c0b03b23a2&results&query=${encodeURIComponent(choice)}&page=1`;
 
   fetch(url)
