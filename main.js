c0fcc5c07f99915e72fe3b3c414e23ecc0827b43
Obinna Ezeka <obinnae815@gmail.com>
2026-02-13 16:47:30 -0800

    creating another fetch for trending movies

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+//Created variable for trending movies API endpoint
+let trendingUrl = 'https://api.themoviedb.org/3/trending/movie/day?api_key=3c111c26b6ae2ffe0c8748c0b03b23a2';
+
+// document.addEventListener('click', getFetchTrending);
+
+fetch(trendingUrl)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data.results)
+    
+    //Creating Section to display trending movies
+    // const
+  })
+  .catch(err => {
+    console.log(`error ${err}`);
+  })
+
+
 // Created event listener for search button to search when clicked
 document.querySelector('#btn').addEventListener('click', getFetch);
 
